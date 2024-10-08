4abd3e1b65529becdeaf53cfdc7ccf36bb2faed5
anton kulakov <kulakov-akv@yandex.ru>
2024-10-08 19:19:12 +0300

    refactor: instead of using a web.xml, add the use of annotations for filters

--- src/main/java/controller/filter/CORSFilter.java
+++ src/main/java/controller/filter/CORSFilter.java
@@ -1,10 +1,12 @@
 package controller.filter;
 
 import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+@WebFilter("/*")
 public class CORSFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
--- src/main/java/controller/filter/EncodingFilter.java
+++ src/main/java/controller/filter/EncodingFilter.java
@@ -1,9 +1,11 @@
 package controller.filter;
 
 import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
 
 import java.io.IOException;
 
+@WebFilter("/*")
 public class EncodingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
