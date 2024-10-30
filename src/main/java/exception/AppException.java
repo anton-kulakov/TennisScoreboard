3110d9f69b3cde3ac28aee05246f3dce027594e8
anton kulakov <kulakov-akv@yandex.ru>
2024-10-30 11:10:53 +0300

    chore: change the ancestor of the AppException

--- src/main/java/exception/AppException.java
+++ src/main/java/exception/AppException.java
@@ -3,7 +3,7 @@ package exception;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
-public class AppException extends Exception {
+public class AppException extends RuntimeException {
     public final int code;
     public final String message;
 }
